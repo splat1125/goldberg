3a6486a3237f212d92f5595f50f5b9400576f2d1
Beck <splat.anonsvn@gmail.com>
2021-02-28 14:54:09 -0500

    init

---
+++ Assets/scripts/ballControl.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ballControl : MonoBehaviour
+{
+    public bool notMoved = true;
+    public float force;
+
+    private cameraFollower anchor;
+
+    private Rigidbody2D myBody;
+    public GameObject cam;
+    public GameObject tgt1;
+    public GameObject tgt2;
+    public GameObject tgt3;
+    public GameObject tgt4;
+    public GameObject tgt5;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        anchor = cam.GetComponent<cameraFollower>();
+        myBody = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(notMoved){
+            if(Input.GetKeyDown(KeyCode.Space)){
+                myBody.AddForce(new Vector2(1, 0) * force);
+                notMoved = false;
+            }
+        }
+
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.name == "checkpoint1"){
+            anchor.anchorTransform = tgt1.transform;
+        }
+        if(other.gameObject.name == "checkpoint2"){
+            anchor.anchorTransform = tgt2.transform;
+        }
+        if(other.gameObject.name == "checkpoint3"){
+            anchor.anchorTransform = tgt3.transform;
+        }
+        if(other.gameObject.name == "checkpoint4"){
+            anchor.anchorTransform = tgt4.transform;
+        }
+        if(other.gameObject.name == "checkpoint5"){
+            anchor.anchorTransform = tgt5.transform;
+        }
+    }
+}
---
+++ Assets/scripts/cameraFollower.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraFollower : MonoBehaviour
+{   //transform position of the camera anchor; this may be changed
+    public Transform anchorTransform;
+    //cam cannot leave here
+    public BoxCollider2D worldBounds;
+
+    //boundary data
+    float xMin, yMin, xMax, yMax;
+    //camera position data
+    float camX, camY;
+
+    //forgot what these do lol
+    float camRatio;
+    float camSize;
+
+    //ref to the camera
+    Camera mainCam;
+
+    //lerp vector
+    Vector3 smoothPos;
+    //lerp coefficient
+    public float smoothRate;
+
+ 
+    // Start is called before the first frame update
+    void Start()
+    {
+        xMin = worldBounds.bounds.min.x;
+        xMax = worldBounds.bounds.max.x;
+        yMin = worldBounds.bounds.min.y;
+        yMax = worldBounds.bounds.max.y;
+
+        mainCam = gameObject.GetComponent<Camera>();
+
+        //height of camera
+        camSize = mainCam.orthographicSize;
+        //width of camera i think
+        camRatio = (xMax + camSize) / 8.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(anchorTransform.position.x);
+        //finding our desired positions within clamps
+        camY = Mathf.Clamp(anchorTransform.position.y, yMin + camSize, yMax - camSize);
+        camX = Mathf.Clamp(anchorTransform.position.x, xMin + camRatio, xMax - camRatio);
+        //finding the lerp between our current and desired position
+        smoothPos = Vector3.Lerp(gameObject.transform.position, new Vector3(camX, camY, gameObject.transform.position.z), smoothRate);
+        //applying the lerp
+        gameObject.transform.position = smoothPos;
+    }
+}
